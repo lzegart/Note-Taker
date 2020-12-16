25bf39c85b3a02641687a69b248316e6f8be3d25
LZ <laraezegart@gmail.com>
2020-12-15 21:43:29 -0800

    added require and app.use for apiRoutes in server.js

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
-// Dependencies
+// Dependencies and exress app setup
 const express = require("express");
-const path = require("path");
-
-// Express app setup
+const apiRoutes = require("./api-routes");
 const app = express();
 const port = 3000;
 
 // Allows express app to handle data parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use("/api", apiRoutes);
 
-// Data
-
-// Routes
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.js"));
-});
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
-});
 
