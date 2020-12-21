47476c068c3e1322fe6b7d0ac1decc902ebaca0a
LZ <laraezegart@gmail.com>
2020-12-21 14:49:01 -0800

    changed api to html-routes and installed and required path in file

--- html-routes.js
+++ html-routes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
+const path = require("path");
 
-router.get("/index", function(req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.js"));
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 router.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // Dependencies and express app setup
 const express = require("express");
-const apiRoutes = require("./api-routes");
+const htmlRoutes = require("./html-routes");
 const app = express();
 const PORT = 3000;
 
 // Allows express app to handle data parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use("/api", apiRoutes);
+app.use(htmlRoutes);
 
 
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
