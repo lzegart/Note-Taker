69a3826aa1f92307d02f0b4bae1f9c01050d2294
LZ <laraezegart@gmail.com>
2020-12-21 15:36:32 -0800

    stated router get and post responses in api-routes that was just created

--- api-routes.js
+++ api-routes.js
@@ -1,17 +1,22 @@
 const router = require("express").Router();
 const path = require("path");
+const fs = require("fs");
 
-router.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-router.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
+router.get("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", function(err, data) {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
 });
 
 router.post("/api/notes", function(req, res) {
     console.log(req.body);
     res.send(200)
 });
-
+// router.post should read file (db.json) to see what's there 
+// add new note to array that was read
+// write new array to db.json
+// ? before adding to array, add id to note (can get randomly generated id)
+// 
 
 module.exports = router
