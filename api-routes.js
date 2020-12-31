81267d4e69c213ea10eed427d945bdaea189c191
LZ <laraezegart@gmail.com>
2020-12-31 00:37:01 -0800

    router.post now reads file db.json to see what's there

--- api-routes.js
+++ api-routes.js
@@ -10,8 +10,12 @@ router.get("/api/notes", function(req, res) {
 });
 
 router.post("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", function(err, data) {
+        if (err) throw err;
+    });
     console.log(req.body);
-    res.send(200)
+    res.send(200);
+
 });
 // router.post should read file (db.json) to see what's there 
 // add new note to array that was read
