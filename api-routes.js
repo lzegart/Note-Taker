c7a009876f8d69bbbf82ab13d1bb0ede8654821e
LZ <laraezegart@gmail.com>
2020-12-31 01:07:55 -0800

    added new note const to array that was read under router.post

--- api-routes.js
+++ api-routes.js
@@ -12,12 +12,19 @@ router.get("/api/notes", function(req, res) {
 router.post("/api/notes", function(req, res) {
     fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", function(err, data) {
         if (err) throw err;
+        const newNote = JSON.parse(data);
+        newNote.push({
+            title: req.body.title,
+            text: req.body.text,
+            id: id,
+        });
     });
+    
     console.log(req.body);
     res.send(200);
-
 });
-// router.post should read file (db.json) to see what's there 
+
+//        router.post should read file (db.json) to see what's there 
 // add new note to array that was read
 // write new array to db.json
 // ? before adding to array, add id to note (can get randomly generated id)
