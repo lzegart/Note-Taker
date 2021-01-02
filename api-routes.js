dcc6618201ec3c9c0c0e0fadbb6c31291835c77a
LZ <laraezegart@gmail.com>
2021-01-02 12:33:07 -0800

    added router.delete function, for of loop, and newArray that pushes new notes to db.json

--- api-routes.js
+++ api-routes.js
@@ -11,29 +11,50 @@ router.get("/api/notes", function(req, res) {
 });
 
 router.post("/api/notes", function(req, res) {
+    console.log("1");
     fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", function(err, data) {
         if (err) throw err;
         const newNote = JSON.parse(data);
         console.log(req.body);
+        console.log("2");
         const id = uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
         newNote.push({
             title: req.body.title,
             text: req.body.text,
             id: id,
         });
+        fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(newNote), function(err, data) {
+            if (err) throw err;
+            console.log("3");
+            console.log("Success!");
+            res.send(200);
+        });
     });
-    fs.writeFile("db", "db.json"), JSON.stringify(newNote), function(err, data) {
-        if (err) throw err;
-    }
-    console.log(req.body);
-    console.log("Success!");
-    res.send(200);
+    console.log("4");
 });
 
-//        router.post should read file (db.json) to see what's there 
-//        add new note to array that was read
-//        write new array to db.json
-// ? before adding to array, add id to note (can get randomly generated id)
-// 
+router.delete("/api/notes/:id", function(req, res) {
+    console.log(req.params.id);
+     fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", function(err, data) {
+         if (err) throw err;
+         const savedNotes = JSON.parse(data);
+         const newArray = [];
+         for (let note of savedNotes) {
+             console.log(note);
+             console.log(note.id === req.params.id);
+             if (note.id !== req.params.id) {
+                newArray.push(note);
+             }
+             
+         }
+         console.log(newArray);
+         fs.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(newArray), function(err, data) {
+            if (err) throw err;
+            console.log("Success!");
+            res.send(200);
+        });
+    });
+});
+
 
 module.exports = router
